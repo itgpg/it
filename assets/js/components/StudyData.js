ca651df53f057d196e7bf24464880c7b50e5464d
itgpg <itgpgwebsite@gmail.com>
2025-03-24 17:08:16 +0530

    feat: Expand StudyData structure with additional subjects and modules for semesters 3 to 6, and update playlist IDs in config

--- assets/js/components/StudyData.js
+++ assets/js/components/StudyData.js
@@ -1,7 +1,5 @@
 class StudyData {
     constructor() {
-        // console.log('StudyData initialized');  // Commented out
-        // Using your existing data structure
         this.data = {
             semester1: {
                 'Python': { 
@@ -22,12 +20,21 @@ class StudyData {
                         }
                     ] 
                 },
+                'Mathematics 1': {
+                    modules: [{ 
+                        name: 'Mathematics 1 Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.maths1 
+                    }]
+                },
+                'Communication Skills': {
+                    modules: [{ 
+                        name: 'Communication Skills Resources', 
+                        files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.communication_skills}`] 
+                    }]
+                },
                 'PYQ': { 
                     modules: [
-                        { 
-                            name: 'CSE PYQ', 
-                            files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.cse}`] 
-                        },
+                        { name: 'CSE PYQ', files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.cse}`] },
                         { name: 'Python Programming PYQ', files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.python}`] },
                         { name: 'ITS PYQ', files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.it}`] },
                         { name: 'ECH PYQ', files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.ech}`] },
@@ -42,45 +49,147 @@ class StudyData {
                         files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.syllabusS2}`] 
                     }] 
                 },
-                'Advance Python': { 
+                'Advanced Python': { 
                     modules: [{ 
-                        name: 'Advance Python Playlist', 
+                        name: 'Advanced Python Playlist', 
                         playlistId: CONFIG.PLAYLIST_IDS.advPython 
                     }] 
                 },
-                'Applied-maths': {
+                'Applied Mathematics': {
                     modules: [{ 
-                        name: 'Applied Maths Playlist', 
-                        playlistId: CONFIG.PLAYLIST_IDS.enginnering_maths 
+                        name: 'Applied Mathematics Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.engineering_maths 
                     }]
                 },
-                
                 'FSD': {
                     modules: [{ 
-                        name: 'FSD', 
+                        name: 'Full Stack Development', 
                         playlistId: CONFIG.PLAYLIST_IDS.FSD
                     }]
                 },
+                'Digital Electronics': {
+                    modules: [{ 
+                        name: 'Digital Electronics Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.digital_electronics 
+                    }]
+                }
+            },
+            semester3: {
+                'Data Structures': { 
+                    modules: [{ 
+                        name: 'Data Structures Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.data_structures 
+                    }] 
+                },
+                'Object-Oriented Programming (OOP)': { 
+                    modules: [{ 
+                        name: 'OOP in C++ Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.oop 
+                    }] 
+                },
+                'Database Management Systems (DBMS)': {
+                    modules: [{ 
+                        name: 'DBMS Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.dbms 
+                    }]
+                },
+                'Operating Systems': {
+                    modules: [{ 
+                        name: 'Operating Systems Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.os 
+                    }]
+                }
+            },
+            semester4: {
+                'Software Engineering': { 
+                    modules: [{ 
+                        name: 'Software Engineering Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.software_engineering 
+                    }] 
+                },
+                'Computer Networks': { 
+                    modules: [{ 
+                        name: 'Computer Networks Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.computer_networks 
+                    }] 
+                },
+                'Web Development': {
+                    modules: [{ 
+                        name: 'Web Development Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.web_dev 
+                    }]
+                },
+                'Cybersecurity Fundamentals': {
+                    modules: [{ 
+                        name: 'Cybersecurity Basics', 
+                        playlistId: CONFIG.PLAYLIST_IDS.cybersecurity 
+                    }]
+                }
+            },
+            semester5: {
+                'Machine Learning': { 
+                    modules: [{ 
+                        name: 'Machine Learning Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.machine_learning 
+                    }] 
+                },
+                'Artificial Intelligence': { 
+                    modules: [{ 
+                        name: 'Artificial Intelligence Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.ai 
+                    }] 
+                },
+                'Cloud Computing': {
+                    modules: [{ 
+                        name: 'Cloud Computing Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.cloud 
+                    }]
+                },
+                'Mobile App Development': {
+                    modules: [{ 
+                        name: 'Mobile App Development', 
+                        playlistId: CONFIG.PLAYLIST_IDS.mobile_dev 
+                    }]
+                }
+            },
+            semester6: {
+                'Blockchain Technology': { 
+                    modules: [{ 
+                        name: 'Blockchain Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.blockchain 
+                    }] 
+                },
+                'Big Data & Analytics': { 
+                    modules: [{ 
+                        name: 'Big Data Analytics Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.big_data 
+                    }] 
+                },
+                'Internet of Things (IoT)': {
+                    modules: [{ 
+                        name: 'IoT Playlist', 
+                        playlistId: CONFIG.PLAYLIST_IDS.iot 
+                    }]
+                },
+                'Capstone Project': {
+                    modules: [{ 
+                        name: 'Project Guidelines & Resources', 
+                        files: [`https://drive.google.com/drive/folders/${CONFIG.FOLDER_IDS.project}`] 
+                    }]
+                }
             }
         };
-        // console.log('Available semesters:', this.getSemesters());  // Commented out
     }
 
     getSemesters() {
-        const semesters = Object.keys(this.data);
-        // console.log('Getting semesters:', semesters);  // Commented out
-        return semesters;
+        return Object.keys(this.data);
     }
 
     getSubjects(semester) {
-        const subjects = Object.keys(this.data[semester] || {});
-        // console.log(`Getting subjects for ${semester}:`, subjects);  // Commented out
-        return subjects;
+        return Object.keys(this.data[semester] || {});
     }
 
     getModules(semester, subject) {
-        const modules = this.data[semester]?.[subject]?.modules || [];
-        // console.log(`Getting modules for ${semester} ${subject}:`, modules);  // Commented out
-        return modules;
+        return this.data[semester]?.[subject]?.modules || [];
     }
-} 
+}
