453c28a0847ad4912a0968e9c0857a0f15d096db
itgpg <itgpgwebsite@gmail.com>
2025-03-24 18:33:28 +0530

    fix: Correct spelling errors in StudyData.js for playlist IDs and module names

--- assets/js/components/StudyData.js
+++ assets/js/components/StudyData.js
@@ -58,12 +58,12 @@ class StudyData {
                 'Applied Mathematics': {
                     modules: [{ 
                         name: 'Applied Mathematics Playlist', 
-                        playlistId: CONFIG.PLAYLIST_IDS.engineering_maths 
+                        playlistId: CONFIG.PLAYLIST_IDS.enginnering_maths 
                     }]
                 },
                 'FSD': {
                     modules: [{ 
-                        name: 'Full Stack Development', 
+                        name: 'fundamental of software development', 
                         playlistId: CONFIG.PLAYLIST_IDS.FSD
                     }]
                 }
